47bace669539fb70f256c06a04ba986db32d35c5
kev127 <kelvinkeya125@gmail.com>
2020-10-12 09:01:14 +0300

    add a setup method that defines instractions

--- user_test.py
+++ user_test.py
@@ -2,3 +2,9 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
+     
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user = User('kelvin', 'keya', '@Y0a!yAv')
