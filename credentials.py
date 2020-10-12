642342ef2aca98243ff7ede58856f9015e402829
kev127 <kelvinkeya125@gmail.com>
2020-10-12 10:49:01 +0300

    add user credentials

--- credentials.py
+++ credentials.py
@@ -3,3 +3,8 @@ class Credentials():
     Create credentials class to help create new objects of credentials
     """
     credentials_list = []
+
+     def __init__(self, user_name, password, email):
+        self.user_name = user_name
+        self.account = account
+        self.password = password
