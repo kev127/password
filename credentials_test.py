2a005a190c9013ee3a8cc50d0dae059ccc04be2c
kev127 <kelvinkeya125@gmail.com>
2020-10-12 11:46:16 +0300

    add method that runs before each credentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,8 @@
 import unittest
 from credential import Credential
+
+def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('yahoo','kev127','@Y0a!yAv')
