4b316fd9530ca5815b43c20a06801abfe650784b
kev127 <kelvinkeya125@gmail.com>
2020-10-12 11:49:35 +0300

    add method that tests if object is initialized

--- credentials_test.py
+++ credentials_test.py
@@ -1,8 +1,21 @@
 import unittest
 from credential import Credential
 
-def setUp(self):
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
         """
         Method that runs before each individual credentials test methods run.
         """
         self.new_credential = Credentials('yahoo','kev127','@Y0a!yAv')
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is properly initialized
+        """
+        self.assertEqual(self.new_credential.account, "account")
+        self.assertEqual(self.new_credential.user_name, "user_name")
+        self.assertEqual(self.new_credential.password, "password")
+
+
+
