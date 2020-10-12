3830ea704c8085a6a965389c8471ddc4705e3776
kev127 <kelvinkeya125@gmail.com>
2020-10-12 10:50:47 +0300

    add save new user credentials

--- credentials.py
+++ credentials.py
@@ -4,7 +4,13 @@ class Credentials():
     """
     credentials_list = []
 
-     def __init__(self, user_name, account, password,):
+    def __init__(self, user_name, account, password,):
         self.user_name = user_name
         self.account = account
         self.password = password
+
+    def save_details(self):
+        """
+        method to store a new credential to the credentials list
+        """
+        Credentials.credentials_list.append(self)
