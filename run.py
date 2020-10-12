eb4b1a30f75bc6ab0f4e199529aa156da0c486f9
kev127 <kelvinkeya125@gmail.com>
2020-10-12 14:22:50 +0300

    add function to create new user credential

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 from user import User
 from credentials import Credentials
 
-def create_user(fname, user_name, password ):
+def create_user(fname, username, password ):
     """
     Function to create a new user
     """
-    new_user = User(fname, user_name, password )
+    new_user = User(fname, username, password )
     return new_user
 
 def save_user(user):
@@ -20,3 +20,10 @@ def display_user():
     Function to display existing user
     """
     return User.display_user()
+
+def create_credential(account,userName,password):
+    """
+    Function to create new user credentials
+    """
+    new_credential = Credential(account,username,password)
+    return new_credential
