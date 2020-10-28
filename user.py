e7d01c11277431091396a019109ffb19c865e809
kev127 <kelvinkeya125@gmail.com>
2020-10-28 21:17:45 +0300

    update class user

--- user.py
+++ user.py
@@ -6,10 +6,12 @@ class User:
 
     user_list = []
 
-    def __init__(self, first_name, last_name, password):
+    def __init__(self,first_name,last_name,user_name,password,confirm_password):
         self.first_name = first_name
         self.last_name = last_name
+        self.user_name = user_name 
         self.password = password
+        self.confirm_password = confirm_password
         
     def save_user_details(self):
         """
